0b8911733206db657b9104ba7b237f934262b72a
Suleman Ahmad <87057938+Sulman23@users.noreply.github.com>
2022-03-13 12:28:34 +0500

    finalized

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ let body = document.querySelector('body');
 // event listner 
 themeChanger.addEventListener("click", () =>{
    themeChanger.classList.toggle('fa-sun'); 
+  
    // checking the theme selection
-  if (themeChanger.classList.contains('fa-sun')) {
-    body.classList.add('active');
-  } else {
-    body.classList.remove('active');
-  }
-})
+    if (themeChanger.classList.contains('fa-sun')) {
+        body.classList.add('active');
+    } else {
+        body.classList.remove('active');
+    }
+});
