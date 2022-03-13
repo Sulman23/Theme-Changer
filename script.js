dec7cbea29fd2d2089fd24897e11d4b8c7f17c65
Suleman Ahmad <87057938+Sulman23@users.noreply.github.com>
2022-03-13 12:02:53 +0500

    almost finished project

---
+++ script.js
@@ -0,0 +1,14 @@
+// Selecting Elements
+let themeChanger = document.getElementById("theme-icon");
+let body = document.querySelector('body');
+
+// event listner 
+themeChanger.addEventListener("click", () =>{
+   themeChanger.classList.toggle('fa-sun'); 
+   // checking the theme selection
+  if (themeChanger.classList.contains('fa-sun')) {
+    body.classList.add('active');
+  } else {
+    body.classList.remove('active');
+  }
+})
